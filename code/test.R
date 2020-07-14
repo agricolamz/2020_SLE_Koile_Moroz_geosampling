e2493cac193422567c03fa5a1542bbe22bf43265
George Moroz <agricolamz@gmail.com>
2020-07-14 21:17:04 +0300

    add fake data

--- code/test.R
+++ code/test.R
@@ -1,20 +1,21 @@
+library(tidyverse)
 # total number of locations
-N <- seq(20, 120, by = 20)
+N <- seq(30, 130, by = 20)
 # type of spatial relations
 spatial_relations <- c("random", "two_regions", "central_periphery")
 # proportion of variation in the explored variable
-p <- seq(0.1, 0.9, by = 0.1)
+p <- seq(0.05, 0.5, by = 0.05)
 # amount of clusters
 k_for_calculation <- seq(0.1, 0.9, by = 0.1)
 # percantage of observations taken from each cluster
 r <- seq(0.1, 0.9, by = 0.1)
 
-df <- expand.grid(N, spatial_relations, p, k_for_calculation, r)
-names(df) <- c("N", "spatial_relations", "p", "k", "r")
+df <- expand.grid(N, p, k_for_calculation, r)
+names(df) <- c("N", "p", "k", "r")
+rm(N, p, r, spatial_relations, k_for_calculation)
 
 df %>% 
   mutate(k = k*N,
-         n = N*r/k,
          id = 1:n()) ->
   df
 
@@ -22,43 +23,60 @@ set.seed(42)
 map_dfr(seq_along(df$N), function(i){
   # create a samle of places
   tibble(id = i,
-         x = rnorm(df$N[i]),
-         y = rnorm(df$N[i]))
+         x = rnorm(df$N[i]*df$p[i], mean = -1),
+         y = rnorm(df$N[i]*df$p[i], mean = 1),
+         value = "a") ->
+    a
+  tibble(id = i,
+         x = rnorm(df$N[i]*(1 - df$p[i])),
+         y = rnorm(df$N[i]*(1 - df$p[i])),
+         value = "b")  %>% 
+    bind_rows(a) %>% 
+    mutate(spatial_relations = "two_regions")
 }) %>% 
   left_join(df) ->
-  results
+  results_regions
 
-library(furrr)
-future::plan(multiprocess)
-map_dfr(seq_along(results$id), function(i){
-  tibble(value = sample(c("a", "b"), 
-                        size = results$N[i], 
-                        prob = c(results$p[i], 1 - results$p[i]), 
-                        replace = TRUE))
+set.seed(42)
+map_dfr(seq_along(df$N), function(i){
+  # create a samle of places
+  tibble(id = i,
+         x = rnorm(df$N[i]*df$p[i]),
+         y = rnorm(df$N[i]*df$p[i]),
+         value = "a") ->
+    a
+  tibble(id = i,
+         x = rnorm(df$N[i]*(1 - df$p[i]), sd = 3),
+         y = rnorm(df$N[i]*(1 - df$p[i]), sd = 3),
+         value = "b")  %>% 
+    bind_rows(a) %>% 
+    mutate(spatial_relations = "central_periphery")
 }) %>% 
-  right_join(results) ->
-  results_2
+  left_join(df) ->
+  results_central
 
-  filter(id == 1000) %>% 
-  ggplot(aes(x, y))+
-  geom_point()
-         
+set.seed(42)
+map_dfr(seq_along(df$N), function(i){
+  # create a samle of places
+  tibble(id = i,
+         x = rnorm(df$N[i]*df$p[i]),
+         y = rnorm(df$N[i]*df$p[i]),
+         value = "a") ->
+    a
+  tibble(id = i,
+         x = rnorm(df$N[i]*(1 - df$p[i])),
+         y = rnorm(df$N[i]*(1 - df$p[i])),
+         value = "b")  %>% 
+    bind_rows(a) %>% 
+    mutate(spatial_relations = "random")
+}) %>% 
+  left_join(df) ->
+  results_random
 
+results_random %>% 
+  bind_rows(results_central, 
+            results_regions) %>% 
+  mutate(id = 1:n()) ->
+  results_all
+rm(results_random, results_central, results_regions, df)
 
-
-
-         value = sample(x = c("a", "b"), 
-                        size = 1,
-                        prob = c(p, 1-p), 
-                        replace = TRUE),
-         Var1 = Var1 + rnorm(N*length(probs)*length(types)),
-         Var2 = Var2 + rnorm(N*length(probs)*length(types)),
-         value = case_when(
-           spatial_relations == "random" ~ value,
-           spatial_relations == "two_regions" ~ ifelse(Var1 > Var2, 
-                                       sample(c(value, "b")), 
-                                       sample(c(value, "a"))),
-           spatial_relations == "central_periphery" ~ ifelse(sqrt((Var1 - N/2)^2 + (Var2 - N/2)^2) > sqrt(2*(N/4)^2), 
-                                     sample(c(value, "b")), 
-                                     sample(c(value, "a"))))) ->
-  results
