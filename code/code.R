27c0245ab7d82dcdd8bc0b282685f72695b58344
George Moroz <agricolamz@gmail.com>
2020-07-14 22:24:30 +0300

    add all data

--- code/code.R
+++ code/code.R
@@ -1,22 +1,18 @@
 library(tidyverse)
 # total number of locations
-N <- seq(30, 130, by = 20)
-# type of spatial relations
-spatial_relations <- c("random", "two_regions", "central_periphery")
+N <- seq(30, 120, by = 30)
 # proportion of variation in the explored variable
-p <- seq(0.05, 0.5, by = 0.05)
+p <- seq(0.1, 0.5, by = 0.1)
 # amount of clusters
-k_for_calculation <- seq(0.1, 0.9, by = 0.1)
 # percantage of observations taken from each cluster
 r <- seq(0.1, 0.9, by = 0.1)
 
-df <- expand.grid(N, p, k_for_calculation, r)
-names(df) <- c("N", "p", "k", "r")
-rm(N, p, r, spatial_relations, k_for_calculation)
+df <- expand.grid(N, p, r)
+names(df) <- c("N", "p", "r")
+rm(N, p, r)
 
 df %>% 
-  mutate(k = k*N,
-         id = 1:n()) ->
+  mutate(id = 1:n()) ->
   df
 
 set.seed(42)
@@ -37,6 +33,16 @@ map_dfr(seq_along(df$N), function(i){
   left_join(df) ->
   results_regions
 
+results_regions %>% 
+  count(id, N, p, value) %>% 
+  filter(value == "a") %>% 
+  mutate(ratio = n/N) %>% 
+  select(id, ratio) %>% 
+  left_join(results_regions) %>% 
+  ggplot(aes(factor(p), ratio))+
+  geom_violin()
+
+
 set.seed(42)
 map_dfr(seq_along(df$N), function(i){
   # create a samle of places
@@ -54,6 +60,15 @@ map_dfr(seq_along(df$N), function(i){
 }) %>% 
   left_join(df) ->
   results_central
+
+results_central %>% 
+  count(id, N, p, value) %>% 
+  filter(value == "a") %>% 
+  mutate(ratio = n/N) %>% 
+  select(id, ratio) %>% 
+  left_join(results_central) %>% 
+  ggplot(aes(factor(p), ratio))+
+  geom_violin()
 
 set.seed(42)
 map_dfr(seq_along(df$N), function(i){
@@ -74,9 +89,43 @@ map_dfr(seq_along(df$N), function(i){
   results_random
 
 results_random %>% 
-  bind_rows(results_central, 
-            results_regions) %>% 
-  mutate(id = 1:n()) ->
-  results_all
-rm(results_random, results_central, results_regions, df)
+  count(id, N, p, value) %>% 
+  filter(value == "a") %>% 
+  mutate(ratio = n/N) %>% 
+  select(id, ratio) %>% 
+  left_join(results_random) %>% 
+  ggplot(aes(factor(p), ratio))+
+  geom_violin()
 
+results_random %>% 
+  filter(id %in% sample(results_random$id, 20)) %>% 
+  ggplot(aes(x, y, color = value))+
+  geom_point()+
+  facet_wrap(~id+p)
+
+results_random %>% 
+  bind_rows(results_central, 
+            results_regions) ->
+  results_all
+# rm(results_random, results_central, results_regions, df)
+
+results_all %>% 
+  count(spatial_relations, id) %>% 
+  select(-n) %>% 
+  mutate(id_new = 1:n()) %>% 
+  left_join(results_all) %>% 
+  mutate(id = id_new) %>% 
+  select(-id_new) ->
+  results_all
+
+results_all %>% 
+  count(id, N, p, spatial_relations, value) %>% 
+  filter(value == "a") %>% 
+  mutate(ratio = n/N) %>% 
+  select(id, ratio) %>% 
+  left_join(results_all) %>% 
+  ggplot(aes(factor(p), ratio))+
+  geom_point()+
+  facet_wrap(~spatial_relations, nrow = 3)
+
+write_csv(results_all, "../data/fake_data.csv")
