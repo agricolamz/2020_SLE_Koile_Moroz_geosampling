5ccd7b61f93dac95ac299e739696e524ff4d8673
George Moroz <agricolamz@gmail.com>
2020-08-05 23:06:26 +0300

    small updates

--- code/2020.08.05.R
+++ code/2020.08.05.R
@@ -373,7 +373,7 @@ scale <- 1
 eq_x4 = c(1, 0, -1, 0)* scale
 eq_y4 = c(0, 1, 0, -1)* scale
 cp_x4 <- c(0, (2*cos(2*pi/3*0:2))) * scale
-cp_y4 <- c(0, (2*sin(2*pi/3*0:3))) * scale
+cp_y4 <- c(0, (2*sin(2*pi/3*0:2))) * scale
 
 map_dfr(1:30, function(i){
   tibble(x = c(rnorm(cl_4_v_30_sample$Var1[i], mean = eq_x4[1]),
@@ -624,19 +624,339 @@ map_dfr(1:70, function(i){
 }) ->
   df_cl_4_v_70
 
+# df_cl_3_v_30 ------------------------------------------------------------
+n_villages <- 30
+n_category <- 3
+X <- as.matrix(expand.grid(1:n_villages, 
+                           1:n_villages)) # repeat n_clusters - 1 times
+X %>% 
+  as_tibble() %>% 
+  mutate(rs = rowSums(.)) %>% 
+  filter(rs <= n_villages) %>% 
+  mutate(Var3 = n_villages-rs) %>% 
+  filter(Var3 > 0) %>% 
+  filter(Var1 >= Var2,
+         Var1 >= Var3,
+         Var2 >= Var3) %>% 
+  select(-rs) %>% 
+  rowwise() %>% 
+  mutate(H = -sum(Var1/n_villages*log2(Var1/n_villages),
+                  Var2/n_villages*log2(Var2/n_villages),
+                  Var3/n_villages*log2(Var3/n_villages))) %>% 
+  arrange(H) %>% 
+  ungroup() %>% 
+  mutate(id = 1:n(),
+         n_villages = n_villages,
+         n_category = n_category) %>% 
+  select(id, n_category, n_villages, H, Var1, Var2, Var3) %>% 
+  slice(round(seq(nrow(.), 1, length.out = 30))) ->
+  cl_3_v_30_sample
+
+scale <- 1
+cp_x3 = c(0, 1, -1)* scale*3/2
+cp_y3 = c(0, 0, 0)* scale*3/2
+eq_x3 <- (2*cos(2*pi/3*0:2)) * scale*2/3
+eq_y3 <- (2*sin(2*pi/3*0:3)) * scale*2/3
+
+map_dfr(1:30, function(i){
+  tibble(x = c(rnorm(cl_3_v_30_sample$Var1[i], mean = eq_x3[1]),
+               rnorm(cl_3_v_30_sample$Var2[i], mean = eq_x3[2]),
+               rnorm(cl_3_v_30_sample$Var3[i], mean = eq_x3[3])),
+         y = c(rnorm(cl_3_v_30_sample$Var1[i], mean = eq_y3[1]),
+               rnorm(cl_3_v_30_sample$Var2[i], mean = eq_y3[2]),
+               rnorm(cl_3_v_30_sample$Var3[i], mean = eq_y3[3])),
+         value = c(rep("Var1", cl_3_v_30_sample$Var1[i]),
+                   rep("Var2", cl_3_v_30_sample$Var2[i]),
+                   rep("Var3", cl_3_v_30_sample$Var3[i])),
+         type = "equadistant") ->
+    eq
+  
+  tibble(x = c(rnorm(cl_3_v_30_sample$Var1[i], mean = cp_x3[1]),
+               rnorm(cl_3_v_30_sample$Var2[i], mean = cp_x3[2]),
+               rnorm(cl_3_v_30_sample$Var3[i], mean = cp_x3[3])),
+         y = c(rnorm(cl_3_v_30_sample$Var1[i], mean = cp_y3[1]),
+               rnorm(cl_3_v_30_sample$Var2[i], mean = cp_y3[2]),
+               rnorm(cl_3_v_30_sample$Var3[i], mean = cp_y3[3])),
+         value = c(rep("Var1", cl_3_v_30_sample$Var1[i]),
+                   rep("Var2", cl_3_v_30_sample$Var2[i]),
+                   rep("Var3", cl_3_v_30_sample$Var3[i])),
+         type = "central-periphery") ->
+    cp
+  
+  tibble(x = c(rnorm(cl_3_v_30_sample$Var1[i], sd = scale),
+               rnorm(cl_3_v_30_sample$Var2[i], sd = scale),
+               rnorm(cl_3_v_30_sample$Var3[i], sd = scale)),
+         y = c(rnorm(cl_3_v_30_sample$Var1[i], sd = scale),
+               rnorm(cl_3_v_30_sample$Var2[i], sd = scale),
+               rnorm(cl_3_v_30_sample$Var3[i], sd = scale)),
+         value = c(rep("Var1", cl_3_v_30_sample$Var1[i]),
+                   rep("Var2", cl_3_v_30_sample$Var2[i]),
+                   rep("Var3", cl_3_v_30_sample$Var3[i])),
+         type = "random") ->
+    rd
+  
+  eq %>% 
+    bind_rows(cp, rd) %>% 
+    mutate(id = cl_3_v_30_sample$id[i],
+           vars = str_c(cl_3_v_30_sample$Var1[i], 
+                        cl_3_v_30_sample$Var2[i], 
+                        cl_3_v_30_sample$Var3[i], 
+                        sep = "-"),
+           n_category = cl_3_v_30_sample$n_category[i],
+           n_villages = cl_3_v_30_sample$n_villages[i],
+           H = cl_3_v_30_sample$H[i])
+}) ->
+  df_cl_3_v_30
+
+
+# df_cl_3_v_50 ------------------------------------------------------------
+n_villages <- 50
+n_category <- 3
+X <- as.matrix(expand.grid(1:n_villages, 
+                           1:n_villages)) # repeat n_clusters - 1 times
+X %>% 
+  as_tibble() %>% 
+  mutate(rs = rowSums(.)) %>% 
+  filter(rs <= n_villages) %>% 
+  mutate(Var3 = n_villages-rs) %>% 
+  filter(Var3 > 0) %>% 
+  filter(Var1 >= Var2,
+         Var1 >= Var3,
+         Var2 >= Var3) %>% 
+  select(-rs) %>% 
+  rowwise() %>% 
+  mutate(H = -sum(Var1/n_villages*log2(Var1/n_villages),
+                  Var2/n_villages*log2(Var2/n_villages),
+                  Var3/n_villages*log2(Var3/n_villages))) %>% 
+  arrange(H) %>% 
+  ungroup() %>% 
+  mutate(id = 1:n(),
+         n_villages = n_villages,
+         n_category = n_category) %>% 
+  select(id, n_category, n_villages, H, Var1, Var2, Var3) %>% 
+  slice(round(seq(nrow(.), 1, length.out = 30))) ->
+  cl_3_v_50_sample
+
+map_dfr(1:30, function(i){
+  tibble(x = c(rnorm(cl_3_v_50_sample$Var1[i], mean = eq_x3[1]),
+               rnorm(cl_3_v_50_sample$Var2[i], mean = eq_x3[2]),
+               rnorm(cl_3_v_50_sample$Var3[i], mean = eq_x3[3])),
+         y = c(rnorm(cl_3_v_50_sample$Var1[i], mean = eq_y3[1]),
+               rnorm(cl_3_v_50_sample$Var2[i], mean = eq_y3[2]),
+               rnorm(cl_3_v_50_sample$Var3[i], mean = eq_y3[3])),
+         value = c(rep("Var1", cl_3_v_50_sample$Var1[i]),
+                   rep("Var2", cl_3_v_50_sample$Var2[i]),
+                   rep("Var3", cl_3_v_50_sample$Var3[i])),
+         type = "equadistant") ->
+    eq
+  
+  tibble(x = c(rnorm(cl_3_v_50_sample$Var1[i], mean = cp_x3[1]),
+               rnorm(cl_3_v_50_sample$Var2[i], mean = cp_x3[2]),
+               rnorm(cl_3_v_50_sample$Var3[i], mean = cp_x3[3])),
+         y = c(rnorm(cl_3_v_50_sample$Var1[i], mean = cp_y3[1]),
+               rnorm(cl_3_v_50_sample$Var2[i], mean = cp_y3[2]),
+               rnorm(cl_3_v_50_sample$Var3[i], mean = cp_y3[3])),
+         value = c(rep("Var1", cl_3_v_50_sample$Var1[i]),
+                   rep("Var2", cl_3_v_50_sample$Var2[i]),
+                   rep("Var3", cl_3_v_50_sample$Var3[i])),
+         type = "central-periphery") ->
+    cp
+  
+  tibble(x = c(rnorm(cl_3_v_50_sample$Var1[i], sd = scale),
+               rnorm(cl_3_v_50_sample$Var2[i], sd = scale),
+               rnorm(cl_3_v_50_sample$Var3[i], sd = scale)),
+         y = c(rnorm(cl_3_v_50_sample$Var1[i], sd = scale),
+               rnorm(cl_3_v_50_sample$Var2[i], sd = scale),
+               rnorm(cl_3_v_50_sample$Var3[i], sd = scale)),
+         value = c(rep("Var1", cl_3_v_50_sample$Var1[i]),
+                   rep("Var2", cl_3_v_50_sample$Var2[i]),
+                   rep("Var3", cl_3_v_50_sample$Var3[i])),
+         type = "random") ->
+    rd
+  
+  eq %>% 
+    bind_rows(cp, rd) %>% 
+    mutate(id = cl_3_v_30_sample$id[i],
+           vars = str_c(cl_3_v_50_sample$Var1[i], 
+                        cl_3_v_50_sample$Var2[i], 
+                        cl_3_v_50_sample$Var3[i], 
+                        sep = "-"),
+           n_category = cl_3_v_50_sample$n_category[i],
+           n_villages = cl_3_v_50_sample$n_villages[i],
+           H = cl_3_v_30_sample$H[i])
+}) ->
+  df_cl_3_v_50
+
+theme_set(theme_bw())
+
+# df_cl_3_v_70 ------------------------------------------------------------
+n_villages <- 70
+n_category <- 3
+X <- as.matrix(expand.grid(1:n_villages, 
+                           1:n_villages)) # repeat n_clusters - 1 times
+X %>% 
+  as_tibble() %>% 
+  mutate(rs = rowSums(.)) %>% 
+  filter(rs <= n_villages) %>% 
+  mutate(Var3 = n_villages-rs) %>% 
+  filter(Var3 > 0) %>% 
+  filter(Var1 >= Var2,
+         Var1 >= Var3,
+         Var2 >= Var3) %>% 
+  select(-rs) %>% 
+  rowwise() %>% 
+  mutate(H = -sum(Var1/n_villages*log2(Var1/n_villages),
+                  Var2/n_villages*log2(Var2/n_villages),
+                  Var3/n_villages*log2(Var3/n_villages))) %>% 
+  arrange(H) %>% 
+  ungroup() %>% 
+  mutate(id = 1:n(),
+         n_villages = n_villages,
+         n_category = n_category) %>% 
+  select(id, n_category, n_villages, H, Var1, Var2, Var3) %>% 
+  slice(round(seq(nrow(.), 1, length.out = 70))) ->
+  cl_3_v_70_sample
+
+map_dfr(1:70, function(i){
+  tibble(x = c(rnorm(cl_3_v_70_sample$Var1[i], mean = eq_x3[1]),
+               rnorm(cl_3_v_70_sample$Var2[i], mean = eq_x3[2]),
+               rnorm(cl_3_v_70_sample$Var3[i], mean = eq_x3[3])),
+         y = c(rnorm(cl_3_v_70_sample$Var1[i], mean = eq_y3[1]),
+               rnorm(cl_3_v_70_sample$Var2[i], mean = eq_y3[2]),
+               rnorm(cl_3_v_70_sample$Var3[i], mean = eq_y3[3])),
+         value = c(rep("Var1", cl_3_v_70_sample$Var1[i]),
+                   rep("Var2", cl_3_v_70_sample$Var2[i]),
+                   rep("Var3", cl_3_v_70_sample$Var3[i])),
+         type = "equadistant") ->
+    eq
+  
+  tibble(x = c(rnorm(cl_3_v_70_sample$Var1[i], mean = cp_x3[1]),
+               rnorm(cl_3_v_70_sample$Var2[i], mean = cp_x3[2]),
+               rnorm(cl_3_v_70_sample$Var3[i], mean = cp_x3[3])),
+         y = c(rnorm(cl_3_v_70_sample$Var1[i], mean = cp_y3[1]),
+               rnorm(cl_3_v_70_sample$Var2[i], mean = cp_y3[2]),
+               rnorm(cl_3_v_70_sample$Var3[i], mean = cp_y3[3])),
+         value = c(rep("Var1", cl_3_v_70_sample$Var1[i]),
+                   rep("Var2", cl_3_v_70_sample$Var2[i]),
+                   rep("Var3", cl_3_v_70_sample$Var3[i])),
+         type = "central-periphery") ->
+    cp
+  
+  tibble(x = c(rnorm(cl_3_v_70_sample$Var1[i], sd = scale),
+               rnorm(cl_3_v_70_sample$Var2[i], sd = scale),
+               rnorm(cl_3_v_70_sample$Var3[i], sd = scale)),
+         y = c(rnorm(cl_3_v_70_sample$Var1[i], sd = scale),
+               rnorm(cl_3_v_70_sample$Var2[i], sd = scale),
+               rnorm(cl_3_v_70_sample$Var3[i], sd = scale)),
+         value = c(rep("Var1", cl_3_v_70_sample$Var1[i]),
+                   rep("Var2", cl_3_v_70_sample$Var2[i]),
+                   rep("Var3", cl_3_v_70_sample$Var3[i])),
+         type = "random") ->
+    rd
+  
+  eq %>% 
+    bind_rows(cp, rd) %>% 
+    mutate(id = cl_3_v_70_sample$id[i],
+           vars = str_c(cl_3_v_70_sample$Var1[i], 
+                        cl_3_v_70_sample$Var2[i], 
+                        cl_3_v_70_sample$Var3[i], 
+                        sep = "-"),
+           n_category = cl_3_v_70_sample$n_category[i],
+           n_villages = cl_3_v_70_sample$n_villages[i],
+           H = cl_3_v_30_sample$H[i])
+}) ->
+  df_cl_3_v_70
+
+
 # merge all  --------------------------------------------------------------
 df_cl_5_v_70 %>% 
   bind_rows(df_cl_5_v_50, df_cl_5_v_30, 
-            df_cl_4_v_70, df_cl_4_v_50, df_cl_4_v_30) ->
-  df_cl_5_4
+            df_cl_4_v_70, df_cl_4_v_50, df_cl_4_v_30,
+            df_cl_3_v_70, df_cl_3_v_50, df_cl_3_v_30) %>% 
+  write_csv("/home/agricolamz/work/materials/2020_SLE_Koile_Moroz_geosampling/data/generated_data.csv")
 
-df_cl_5_4 %>% 
-  count(n_villages, type, n_category)
+rm(list = ls())
 
+df_cl <- read_csv("/home/agricolamz/work/materials/2020_SLE_Koile_Moroz_geosampling/data/generated_data.csv")
 
-df_cl_5_4 %>% 
-  filter(id > 9000, n_category == 5) %>% 
-  ggplot(aes(x, y, color = value, shape = value))+
-  geom_point(size = 3)+
-  stat_ellipse()+
-  facet_grid(type~round(H, 2))
+df_cl %>% 
+  count(type, n_category, n_villages, vars) %>% 
+  mutate(dataset_id  = 1:n()) %>% 
+  left_join(df_cl[, -5]) %>% 
+  select(dataset_id, type, n_category, n_villages, vars, x, y, value, H) %>% 
+  write_csv("/home/agricolamz/work/materials/2020_SLE_Koile_Moroz_geosampling/data/generated_data.csv")
+
+# analyze -----------------------------------------------------------------
+df_cl <- read_csv("/home/agricolamz/work/materials/2020_SLE_Koile_Moroz_geosampling/data/generated_data.csv")
+
+set.seed(42)
+map_dfr(seq(0.05, 0.9, 0.05), function(i){
+  df_cl %>% 
+    group_by(dataset_id) %>% 
+    mutate(kmeans_cluster = kmeans(as.matrix(tibble(x, y)), centers = unique(n_villages*i))$cluster) %>% 
+    group_by(dataset_id, kmeans_cluster) %>% 
+    sample_n(1) %>% 
+    mutate(proportion_of_village = i,
+           cluster_type = "k-means") %>% 
+    ungroup() %>% 
+    select(-kmeans_cluster)
+}) ->
+  kmeans_result
+
+set.seed(42)
+map_dfr(seq(0.05, 0.9, 0.05), function(i){
+  df_cl %>% 
+    group_by(dataset_id) %>% 
+    mutate(hclust_cluster = cutree(hclust(dist(tibble(x, y))), k = unique(n_villages*i))) %>% 
+    group_by(dataset_id, hclust_cluster) %>% 
+    sample_n(1) %>% 
+    mutate(proportion_of_village = i,
+           cluster_type = "hierarchical clustering") %>% 
+    ungroup() %>% 
+    select(-hclust_cluster)
+}) ->
+  hclust_result
+
+set.seed(42)
+map_dfr(seq(0.05, 0.9, 0.05), function(i){
+  df_cl %>% 
+    group_by(dataset_id) %>% 
+    sample_n(unique(n_villages*i)) %>% 
+    mutate(proportion_of_village = i,
+           cluster_type = "random")
+}) ->
+  random_result
+
+random_result %>% 
+  bind_rows(hclust_result, kmeans_result) %>% 
+  group_by(type, n_category, n_villages, cluster_type, proportion_of_village) %>% 
+  mutate(H = H/max(H)) %>% 
+  ungroup() %>% 
+  distinct(proportion_of_village, dataset_id, type, value, H, n_category, n_villages, cluster_type) %>% 
+  count(dataset_id, H, n_category, type, proportion_of_village, n_villages, cluster_type) %>% 
+  mutate(ratio = n/n_category) %>% 
+  ggplot(aes(proportion_of_village, ratio, color = cluster_type))+
+  #geom_jitter(alpha = 0.5, size = 0.7)+
+  geom_smooth(se = FALSE)+
+  xlim(0.05, 0.6)+
+  facet_grid(n_villages+n_category~type)
+
+  
+random_result %>% 
+  bind_rows(hclust_result, kmeans_result) %>% 
+  group_by(type, n_category, n_villages, cluster_type, proportion_of_village, vars) %>% 
+  ungroup() %>% 
+  distinct(proportion_of_village, dataset_id, type, value, H, n_category, n_villages, cluster_type, vars) %>% 
+  count(dataset_id, H, n_category, type, proportion_of_village, n_villages, cluster_type, vars) %>% 
+  mutate(ratio = n/n_category) %>% 
+  select(-n) %>% 
+  pivot_wider(values_from = ratio, names_from = cluster_type) %>% 
+  mutate(hc = `hierarchical clustering` - random,
+         km = `k-means` - random) %>%
+  pivot_longer(values_to = "value", names_to = "cluster_type", hc:km) %>% 
+  ggplot(aes(proportion_of_village, value, color = cluster_type))+
+  geom_smooth(se = FALSE)+
+  xlim(0.05, 0.6)+
+  facet_grid(n_villages+n_category~type)+
+  geom_hline(yintercept = 0, linetype = 2)
+
